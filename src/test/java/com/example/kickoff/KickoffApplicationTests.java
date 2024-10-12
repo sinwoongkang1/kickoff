359e977b253a3efb8300f5ba57bbb1cb8def2630
sinwoongkang1 <kkace22@gmail.com>
2024-10-12 16:57:53 +0900

    init setting

---
+++ src/test/java/com/example/kickoff/KickoffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kickoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickoffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
